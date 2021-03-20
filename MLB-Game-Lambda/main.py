12b9ca15865853b37a3a9d96fc305a2feb64eeb3
David James Skudra <djskudra@uwaterloo.ca>
2021-03-20 16:12:31 -0400

    Got functioning get player data call implemented

--- MLB-Game-Lambda/main.py
+++ MLB-Game-Lambda/main.py
@@ -1,30 +1,33 @@
 import json
-import mlbgame
+import statsapi
+from datetime import datetime
 
 
+# TODO return proper 400 for player not found for year, work on year formatting for getting player id
 def lambda_handler(event, context):
-    players = event["multiValueQueryStringParameters"]['playername']
-    print("Started to retrieve live player data with player list {}".format(players))
+    player_name = event["queryStringParameters"]['player_name']
+    year = event["queryStringParameters"]['year']
+    stat_group = event["queryStringParameters"]['stat_group']
 
-    # Hardcode for now to test since season isnt live
-    year = 2019
-    month = 7
-    day = 24
+    print("Started to retrieve player data for player {} in year {}".format(player_name, year))
+    updated_year = 'career' if ('~' == year) else 'yearByYear'
+    formatted_stat_group = '[{0}]'.format(stat_group)
+    last_year = datetime.now().year - 1 # Cant use current year until season starts
 
-    league_info = mlbgame.league()
-    jays_july_2019 = mlbgame.games(year, months=month, home='Blue Jays', away='Blue Jays')
-    game_id = jays_july_2019[0][0].game_id
-    game_events = mlbgame.game_events(game_id)
-    league_info.nice_output()
+    person_id = statsapi.lookup_player(player_name, season=last_year)[0].get('id')
+    player_data = statsapi.player_stat_data(person_id, group=formatted_stat_group, type=updated_year)
 
+    if updated_year == 'yearByYear':
+        filtered_year_list = [stat_year for stat_year in player_data.get('stats') if stat_year.get('season') == year]
+        player_data['stats'] = filtered_year_list
+
+    print("Successfully Returning player data for player {}".format(player_name))
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(player_data)
     }
 
-
-if __name__ == '__main__':
-    playername_list = ['Chris Sale', 'Gerrit Cole']
-    playername = dict(playername=playername_list)
-    event = dict(multiValueQueryStringParameters=playername)
-    lambda_handler(event, 'yeet')
+#if __name__ == '__main__':
+#    query_params = dict(player_name='Cole, Gerrit', year='2019', stat_group='pitching')
+#    event = dict(queryStringParameters=query_params)
+#    lambda_handler(event, 'yeet')
