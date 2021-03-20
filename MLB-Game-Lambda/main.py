708240cfe592c0f2a0989e6d67ad95bc93a65461
David James Skudra <djskudra@uwaterloo.ca>
2021-03-20 17:09:44 -0400

    Corrected for query parameter exception, api gateway working this iteration

--- MLB-Game-Lambda/main.py
+++ MLB-Game-Lambda/main.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 # TODO return proper 400 for player not found for year, work on year formatting for getting player id
 def lambda_handler(event, context):
-    player_name = event["queryStringParameters"]['player_name']
-    year = event["queryStringParameters"]['year']
-    stat_group = event["queryStringParameters"]['stat_group']
+    player_name = event['queryStringParameters']['player_name']
+    year = event['queryStringParameters']['year']
+    stat_group = event['queryStringParameters']['stat_group']
 
-    print("Started to retrieve player data for player {} in year {}".format(player_name, year))
+    print('Started to retrieve player data for player {} in year {}'.format(player_name, year))
     updated_year = 'career' if ('~' == year) else 'yearByYear'
     formatted_stat_group = '[{0}]'.format(stat_group)
     last_year = datetime.now().year - 1 # Cant use current year until season starts
@@ -21,7 +21,7 @@ def lambda_handler(event, context):
         filtered_year_list = [stat_year for stat_year in player_data.get('stats') if stat_year.get('season') == year]
         player_data['stats'] = filtered_year_list
 
-    print("Successfully Returning player data for player {}".format(player_name))
+    print('Successfully Returning player data for player {}'.format(player_name))
     return {
         'statusCode': 200,
         'body': json.dumps(player_data)
